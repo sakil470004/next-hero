032a4e610a780a32fcfbda5f3e379c0b7fe48f65
Mynul Islam <mynul.sakil@gmail.com>
2025-01-14 20:08:08 +0600

    v 3

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,23 @@
 import AboutContents from '@/components/AboutContents'
 import React from 'react'
 
-function AboutPage() {
+const getTime = async() => {
+const res = await fetch('http://localhost:3000/time',{
+  next:{
+    revalidate:5,
+  }
+});
+const data = await res.json();
+return data.currentTime
+};
+ async function AboutPage() {
+  const currentTime= await getTime();
   return (
     <div>
         <AboutContents/>
+        <div className="text-center text-2xl mt-10">
+          Current Time: {currentTime}
+          </div>
     </div>
   )
 }
---
+++ src/app/time/route.js
@@ -0,0 +1,7 @@
+// for remove catch behavior, you can use force-dynamic
+// export const dynamic = 'force-dynamic';
+export async function GET(){
+    return Response.json({
+       currentTime: new Date().toLocaleTimeString()
+    })
+}
