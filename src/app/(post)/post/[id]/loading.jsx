e907f91625da151bb300096706388555e32ae785
Mynul Islam <mynul.sakil@gmail.com>
2025-01-15 13:16:59 +0600

    adding v4

---
+++ src/app/(post)/post/[id]/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div>POST Details is Loading</div>
+  )
+}
+
+export default loading
---
+++ src/app/(post)/post/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div>POST Data is Loading</div>
+  )
+}
+
+export default loading
