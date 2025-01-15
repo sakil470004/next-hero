be63190ecdc1c5475ea2fa08f352adec6f304532
Mynul Islam <mynul.sakil@gmail.com>
2025-01-15 14:22:12 +0600

    for build

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,33 +1,25 @@
-import AboutContents from '@/components/AboutContents';
-import React from 'react';
+import AboutContents from '@/components/AboutContents'
+import React from 'react'
 
-export async function getServerSideProps() {
-  try {
-    const res = await fetch('http://localhost:3000/time');
-    if (!res.ok) {
-      throw new Error('Failed to fetch time');
-    }
-    const data = await res.json();
-    return {
-      props: { currentTime: data.currentTime || 'N/A' },
-    };
-  } catch (error) {
-    console.error('Error fetching time:', error.message);
-    return {
-      props: { currentTime: 'Error fetching time' },
-    };
-  }
-}
-
-function AboutPage({ currentTime }) {
+// const getTime = async () => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/time`, {
+//     next: {
+//       revalidate: 5,
+//     }
+//   });
+//   const data = await res.json();
+//   return data.currentTime
+// };
+async function AboutPage() {
+  // const currentTime = await getTime();
   return (
     <div>
       <AboutContents />
       <div className="text-center text-2xl mt-10">
-        Current Time: {currentTime}
+        {/* Current Time: {currentTime} */}
       </div>
     </div>
-  );
+  )
 }
 
-export default AboutPage;
+export default AboutPage
--- src/app/time/route.js
+++ src/app/time/route.js
@@ -1,5 +1,5 @@
 // for remove catch behavior, you can use force-dynamic
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 export async function GET(){
     return Response.json({
        currentTime: new Date().toLocaleTimeString()
