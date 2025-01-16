b1db7af2e91748f9f0be7cff3f9910d0fb347b07
Mynul Islam <mynul.sakil@gmail.com>
2025-01-16 21:38:31 +0600

    finished v2

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+const handler = NextAuth({
+    session: {
+        strategy: 'jwt',
+    },
+    providers:[
+        
+    ]
+
+})
+
+export { handler as GET, handler as POST }
+
